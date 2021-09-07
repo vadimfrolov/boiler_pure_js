a71c13a6c924facc9ddba1a631a9e13d757873f5
Vadim Frolov <vadimf74@ya.ru>
2021-09-07 20:53:49 +0300

    include laptop image

--- webpack.config.js
+++ webpack.config.js
@@ -32,15 +32,22 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          "style-loader",
+          'style-loader',
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
-              importLoaders: 1
-            }
+              importLoaders: 1,
+            },
           },
-          "postcss-loader"
-        ]
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '/public/laptop.png',
+        },
       },
     ],
   },

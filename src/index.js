f95496507554b60e55b6133d0addcf2b2e5f636f
Vadim Frolov <vadimf74@ya.ru>
2021-09-22 18:42:28 +0300

    interactive cards

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import git from '../public/git.png';
 import link from '../public/link.png';
 import inst from '../public/inst.png';
 
-
-
 function component() {
   const element = document.createElement('img');
   element.classList.add('laptop-img');
@@ -41,6 +39,55 @@ function instImg() {
   element.src = inst;
   return element;
 }
+
+const buttons = document.querySelectorAll('input[type=radio]');
+const textButton = document.getElementById('card-text');
+for (let i = 0; i < buttons.length; i += 1) {
+  buttons[i].addEventListener('click', () => {
+    switch (buttons[i].id) {
+      case '1':
+        textButton.innerText = 'text1';
+        break;
+      case '2':
+        textButton.innerText = 'text2';
+        break;
+      case '3':
+        textButton.innerText = 'text3';
+        break;
+      case '4':
+        textButton.innerText = 'text4';
+        break;
+        case '5':
+          textButton.innerText = 'text5';
+          break;
+      default:
+        console.log('goodbye');
+        console.log(buttons[i].id);
+    }
+  });
+}
+
+// const ux = document.getElementById("3");
+
+// ux.onclick = function () {
+//   console.log(ux[0].checked);
+// }
+
+// const radios = document.querySelectorAll("input");
+// for (let i=0; i <= radios.length; i+= 1) {
+//   radios[i].onclick = function () {
+//     if (radios[i].checked) {
+//       console.log(radios[i].id)
+//     }
+//     // const rbs = document.querySelectorAll('input[name="choice"]');
+//     // let selectedValue;
+//     // for (const rb of rbs) {
+//     //     if (rb.checked) {
+//     //         selectedValue = rb.value;
+//     //         break;
+//     //     }
+//     }
+// }
 
 document.getElementById('front-inside').appendChild(component());
 document.getElementById('central-logo-text').appendChild(avatar());

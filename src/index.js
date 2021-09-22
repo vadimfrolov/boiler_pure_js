7e883b630b6edc46ffc9d07bf37f23d62a425455
Vadim Frolov <vadimf74@ya.ru>
2021-09-22 15:33:22 +0300

    contact links

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './styles.css';
 import laptop from '../public/laptop.png';
 import avatarImage from '../public/avatar11r.png';
+import git from '../public/git.png';
+import link from '../public/link.png';
+import inst from '../public/inst.png';
+
+
 
 function component() {
   const element = document.createElement('img');
@@ -16,5 +21,30 @@ function avatar() {
   return element;
 }
 
+function gitImg() {
+  const element = document.createElement('img');
+  element.classList.add('social-icon');
+  element.src = git;
+  return element;
+}
+
+function linkImg() {
+  const element = document.createElement('img');
+  element.classList.add('social-icon');
+  element.src = link;
+  return element;
+}
+
+function instImg() {
+  const element = document.createElement('img');
+  element.classList.add('social-icon');
+  element.src = inst;
+  return element;
+}
+
 document.getElementById('front-inside').appendChild(component());
 document.getElementById('central-logo-text').appendChild(avatar());
+
+document.getElementById('git').appendChild(gitImg());
+document.getElementById('link').appendChild(linkImg());
+document.getElementById('inst').appendChild(instImg());
